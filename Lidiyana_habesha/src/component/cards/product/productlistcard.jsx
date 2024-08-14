465bbe285d7d363da4bc6ada4a3118439c15eb74
Yabe12 <yeabsirabehailu92@gmail.com>
2024-08-14 09:56:31 +0300

    the product page modification

--- Lidiyana_habesha/src/component/cards/product/productlistcard.jsx
+++ Lidiyana_habesha/src/component/cards/product/productlistcard.jsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 import { Rating } from "@mui/material";
-import styles from "./ProductListCard.module.css"; // Assuming you have a CSS module
+
+import styles from "./ProductListCard.module.css"; 
 
 const ProductListCard = ({ image, text }) => {
   const [rating, setRating] = useState(0);
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(prevCount => prevCount + 1); 
+  };
+
+  const handleDecrement = () => {
+    setCount(prevCount => (prevCount > 1 ? prevCount - 1 : 1)); 
+  };
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    if (value >= 1) {
+      setCount(value);
+    }
+  };
 
   return (
     <div className={styles.card}>
@@ -13,13 +30,45 @@ const ProductListCard = ({ image, text }) => {
       <div className={styles.text}>
         <span className={styles.productName}>{text}</span>
       </div>
-      <div className={styles.icon}>🛒</div>
-      <Rating
-         value={rating}
-        onChange={(event, newValue) => {
-          setRating(newValue);
+      {/* Uncomment if needed
+      <button
+        className={styles.icon}
+        onClick={handleClick}
+        style={{
+          backgroundColor: isClicked ? 'green' : 'white',
+          color: isClicked ? 'white' : 'black', 
         }}
-      />
+      >
+        🛒
+      </button>
+      */}
+      <div className={styles.cart_button}>
+        <div className={styles.count}>
+          <button
+            className={styles.counter_button}
+            id="decrement"
+            onClick={handleDecrement}
+          >
+            -
+          </button>
+          <input
+            type="number"
+            value={count}
+            onChange={handleInputChange}
+            min="1"
+          />
+          <button
+            className={styles.counter_button}
+            id="increment"
+            onClick={handleIncrement}
+          >
+            +
+          </button>
+        </div>
+        <div className={styles.add_cart}>
+          <button className={styles.button}>Add to cart</button>
+        </div>
+      </div>
     </div>
   );
 };
