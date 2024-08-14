6d638c710c8d8fbfd53e0c96703e1a5d0723900c
Yabe12 <yeabsirabehailu92@gmail.com>
2024-08-14 10:34:36 +0300

    modify home page

--- Lidiyana_habesha/src/component/Header/Header.jsx
+++ Lidiyana_habesha/src/component/Header/Header.jsx
@@ -1,18 +1,30 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import LogoSection from '../logo/LogoSection';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo}>
           LIDIYANA_HABEHA
         </div>
-        {/* <LogoSection/> */}
+        <div
+          className={`${styles.hamburger} ${menuOpen ? styles.active : ''}`}
+          onClick={toggleMenu}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <nav>
-          <ul className={styles.navList}>
+          <ul className={`${styles.navList} ${menuOpen ? styles.active : ''}`}>
             <li><a href="#home">Home</a></li>
             <li><a href="#product">Product</a></li>
             <li><a href="#about">About</a></li>
