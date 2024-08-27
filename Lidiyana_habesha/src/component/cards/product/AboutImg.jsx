2bd5d7889bfc1a70260de446058ca0459c975b25
Yabe12 <yeabsirabehailu92@gmail.com>
2024-08-27 11:43:17 +0300

    style is changed

--- Lidiyana_habesha/src/component/cards/product/AboutImg.jsx
+++ Lidiyana_habesha/src/component/cards/product/AboutImg.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import styles from './Card.module.css';
-import myImage from '../../../assets/images/about.png';
+import React from "react";
+import styles from "./AboutImg.module.css";
+import myImage from "../../../assets/images/about.png";
 
-const Card = () => {
+const AboutImg = () => {
   return (
     <div className={styles.imageSection}>
-    <div className={styles.imageContainer}>
-    <div className={styles.image}><img src={myImage} alt="Description" /></div>
+      <div className={styles.imageContainer}>
+        <div className={styles.image}>
+          <img src={myImage} alt="Description" />
+        </div>
+      </div>
     </div>
-  </div>
   );
 };
 
-export default Card;
+export default AboutImg;
--- Lidiyana_habesha/src/page/About/About.jsx
+++ Lidiyana_habesha/src/page/About/About.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import styles from './About.module.css';
-import ImageSection from '../../component/cards/product/AboutImg';
+import React from "react";
+import styles from "./About.module.css";
 
+import ImageSection from "../../component/cards/product/AboutImg";
 
 const About = () => {
   return (
-  <div className="div">
-
-
- <div className={styles.aboutUsContainer}>
-
- <div className={styles.text}>
- <h1 className={styles.heading}>ABOUT US</h1>
-      <p className={styles.paragraph}>Welcome to lidiyana habesha! We are passionate about providing you with the best clothing and fashion accessories. Our mission is to offer high-quality products that make you look and feel great.</p>
-      <div className={styles.nameofower}>Founder & CEO @lidiana_marketing_firm
-      Founder, Creative director @lidiana_habesha</div>
-      <h2 className={styles.heading}>OUT STORY</h2>
-      <p className={styles.paragraph}>[Your Store Name] was founded in [Year] with the goal of bringing unique and stylish clothing to fashion enthusiasts around the world. We believe in quality, style, and customer satisfaction.</p>
-  
-     
- </div>
- <div className={styles.img}>< ImageSection/></div>
+    <div className="div">
+      <div className={styles.aboutUsContainer}>
+        <div className={styles.text}>
+          <h1 className={styles.heading}>ABOUT US</h1>
+          <p className={styles.paragraph}>
+            Welcome to lidiyana habesha! We are passionate about providing you
+            with the best clothing and fashion accessories. Our mission is to
+            offer high-quality products that make you look and feel great.
+          </p>
+          <div className={styles.nameofower}>
+            Founder & CEO @lidiana_marketing_firm Founder, Creative director
+            @lidiana_habesha
+          </div>
+          <h2 className={styles.heading}>OUT STORY</h2>
+          <p className={styles.paragraph}>
+            [Your Store Name] was founded in [Year] with the goal of bringing
+            unique and stylish clothing to fashion enthusiasts around the world.
+            We believe in quality, style, and customer satisfaction.
+          </p>
+        </div>
+        <div className={styles.img}>
+          <ImageSection />
+        </div>
+      </div>
     </div>
-  </div>
   );
 };
 
 export default About;
-
-
